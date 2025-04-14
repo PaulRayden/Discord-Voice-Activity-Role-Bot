587876e5ccb8798f7c364556c10faed5270f288a
PaulRayden <35412322+PaulRayden@users.noreply.github.com>
2025-04-14 20:50:54 -0300

    Update callback.php
    Actualizado el Location del header y los href hacia el index.

--- callback.php
+++ callback.php
@@ -38,20 +38,20 @@
 
             if (isset($userData['id']) && $userData['id'] == $adminUserId) {
                 // El usuario es el administrador, guardar información en la sesión e ir al panel
-                $_SESSION['admin_logged_in'] = true;
-                header('Location: panel.php');
+                $_SESSION['admin_logged_in'] = true;                
+                header('Location: php/panel/panel.php');
                 die();
             } else {
                 // El usuario no es el administrador, mostrar mensaje de error
-                echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">No tienes permiso para acceder a este panel.</div><p><a href="index.php">Volver a la página de inicio</a></p></div>';
+                echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">No tienes permiso para acceder a este panel.</div><p><a href="../index.php">Volver a la página de inicio</a></p></div>';
             }
         } else {
             // Error al obtener el token de acceso
-            echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Error al iniciar sesión con Discord.</div><p><a href="index.php">Volver a la página de inicio</a></p></div>';
+            echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Error al iniciar sesión con Discord.</div><p><a href="../index.php">Volver a la página de inicio</a></p></div>';
         }
     } else {
         // No se recibió el código de autorización
-        echo '<div class="container mt-5"><div class="alert alert-warning" role="alert">Se produjo un error durante la autenticación.</div><p><a href="index.php">Volver a la página de inicio</a></p></div>';
+        echo '<div class="container mt-5"><div class="alert alert-warning" role="alert">Se produjo un error durante la autenticación.</div><p><a href="../index.php">Volver a la página de inicio</a></p></div>';
     }
 ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">

4b35720cdcfec336e6b70deeaa1b57ab27e61dbf
PaulRayden <35412322+PaulRayden@users.noreply.github.com>
2025-04-17 19:40:15 -0300

    Create footer.php

---
+++ theme/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-light py-3 mt-5 text-center">
+        <div class="container">
+            <p>&copy; <?php echo date("Y"); ?> Paul Rayden / PREL - Agency</p>
+        </div>
+    </footer>
+</body>

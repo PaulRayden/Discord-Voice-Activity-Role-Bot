bceb3045ecb7db59a8a015c354c2e3ac9b98821e
PaulRayden <35412322+PaulRayden@users.noreply.github.com>
2025-04-17 20:11:15 -0300

    Update header.php
    fix dir assets

--- theme/header.php
+++ theme/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-auto">
-                <img src="assets/images/logo-ds-bot-monitor_x250px.webp" alt="Logo" class="img-fluid" style="max-height: 80px;">
+                <img src="../assets/images/logo-ds-bot-monitor_x250px.webp" alt="Logo" class="img-fluid" style="max-height: 80px;">
             </div>
         </div>
     </div>

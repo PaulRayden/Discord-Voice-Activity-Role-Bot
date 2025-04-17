41e2550738dc25d58ca98107a58da257791a9dcf
PaulRayden <35412322+PaulRayden@users.noreply.github.com>
2025-04-17 19:38:47 -0300

    Create header.php

---
+++ theme/header.php
@@ -0,0 +1,9 @@
+<header class="bg-light py-3">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <img src="assets/images/logo.png" alt="Logo" class="img-fluid" style="max-height: 80px;">
+            </div>
+        </div>
+    </div>
+</header>
